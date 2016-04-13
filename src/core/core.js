32d2218705652a16dbc90e36355c7206421a9c53
Kos <kosich@users.noreply.github.com>
2016-04-13 17:15:59 +0300

    SH-28 basic structure (#1)
    moved files under /src fldr
    added mqtt plugins
    changed main file path in packagejson

---
+++ src/core/core.js
@@ -0,0 +1,25 @@
+/*
+ this is core API, that would be consumed by internal system as well as
+ by public API providers
+*/
+
+/* STUB {{{
+  
+export {
+  deviceManager: {
+    list,
+    get: ()=>{
+      return {
+        read,
+        write
+      }
+    },
+    add,
+    remove,
+    update
+  },
+
+  settingsManager
+};
+
+}}} */
