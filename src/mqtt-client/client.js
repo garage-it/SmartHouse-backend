3429eb4cf773a24ea74b897141cb2f4cf20300f7
Dmytro Tolok <Dmytro_Tolok@epam.com>
2016-05-19 17:00:42 +0300

    [TG-72] Created functionality for outputting to MQTT

--- src/mqtt-client/client.js
+++ src/mqtt-client/client.js
@@ -7,10 +7,12 @@ import config from '../config/env';
 import mqtt from 'mqtt';
 import Debugger from 'debug';
 import input from '../data-streams/input';
+import output from '../data-streams/output';
 
 const debug = Debugger('mqtt-client');
 
 const EVENT_NAME = 'event';
+const MQTT_PATH = '/smart-home/in/';
 
 // Create a client connection
 const client = mqtt.connect({
@@ -30,5 +32,9 @@ function onSubscribed() {
         let message = JSON.parse(rawMessage);
         debug(`got message: topic '${topic}', message: '${message}'`);
         input.write(message);
+    });
+
+    output.stream.subscribe((message) => {
+        client.publish(MQTT_PATH + message.device, message.data);
     });
 }
