0397677298e561c8a941c9235e74c86b94a02a85
Dmitry Golysh <disist@users.noreply.github.com>
2016-05-20 12:02:47 +0300

    [TG-29] Integrate with real data and integrate for display all sensor widgets (#9)
    * [TG-29] Integrate with real data and integrate for display all sensor widgets
    
    * [TG-29] [Refactor] Subscribe only on out

--- src/mqtt-client/client.js
+++ src/mqtt-client/client.js
@@ -10,13 +10,14 @@ import input from '../data-streams/input';
 
 const debug = Debugger('mqtt-client');
 
-const EVENT_NAME = 'event';
+const EVENT_NAME = '/smart-home/out/#';
 
 // Create a client connection
+
 const client = mqtt.connect({
-    host: config.mqtt.host,
+    host: config.mqtt.hostname,
     port: config.mqtt.port,
-    auth: `${config.mqtt.user}:${config.mqtt.pass}`
+    auth: `${config.mqtt.username}:${config.mqtt.password}`
 });
 
 client.on('connect', onConnect);
@@ -27,8 +28,14 @@ function onConnect() {
 
 function onSubscribed() {
     client.on('message', function(topic, rawMessage) {
-        let message = JSON.parse(rawMessage);
+        let message = {
+            device: topic.split('/').pop(),
+            value: rawMessage.toString()
+        };
+        
+        console.log(`got message: topic '${topic}', message: '${rawMessage.toString()}'`); // eslint-disable-line
         debug(`got message: topic '${topic}', message: '${message}'`);
         input.write(message);
+
     });
 }
