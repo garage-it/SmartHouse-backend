34a35bf068440af012e0d43c2e862c59df40e62e
Oleksandr Holdfarb <oleksandr_holdfarb@epam.com>
2016-05-20 18:20:08 +0300

    [TG-81][feat] Back end for adding and editing scenario

--- src/API/scenarios/scenario.test.js
+++ src/API/scenarios/scenario.test.js
@@ -12,7 +12,7 @@ describe('## scenario APIs', () => {
     describe('# GET /api/scenarios/:scenarioId', () => {
         it('should get scenario details', (done) => {
             request(app)
-                .get(`/api/scenarios/${seedData.scenarios[0]._id}`)
+                .get(`/api/scenarios/${seedData.scenarios[0].id}`)
                 .expect(httpStatus.OK)
                 .then(res => {
                     expect(res.body).to.deep.equal(seedData.scenarios[0]);
@@ -32,5 +32,36 @@ describe('## scenario APIs', () => {
                 });
         });
     });
+
+    describe('# PUT /api/scenarios/:id', () => {
+        it('should update scenario', (done) => {
+            const originalScenario = seedData.scenarios[0],
+                modifiedScenario = Object.assign({}, originalScenario, {description: 'new description'});
+            request(app)
+                .put(`/api/scenarios/${originalScenario.id}`)
+                .send(modifiedScenario)
+                .expect(httpStatus.OK)
+                .then(res => {
+                    expect(res.body).to.deep.equal(modifiedScenario);
+                    done();
+                });
+        });
+    });
+
+    describe('# POST /api/scenarios', () => {
+        it('should update scenario', (done) => {
+            const scenario = Object.assign({}, seedData.scenarios[0]);
+            delete scenario.id;
+            request(app)
+                .post('/api/scenarios')
+                .send(scenario)
+                .expect(httpStatus.OK)
+                .then(res => {
+                    expect(res.body).to.contain(scenario);
+                    done();
+                });
+        });
+    });
+
 
 });
