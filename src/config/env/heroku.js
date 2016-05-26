9b44aee7bf5e09447aecf752eea246fb591749c1
Dmytro Golysh <disist357@gmail.com>
2016-05-26 14:47:26 +0300

    [TG-12][Feature] Add deploy to heroku

---
+++ src/config/env/heroku.js
@@ -0,0 +1,12 @@
+export default {
+    env: 'heroku',
+    db: process.env.MONGO,
+    port: process.env.PORT,
+    seedDB: false,
+    mqtt: {
+        port: process.env.MQTT_PORT,
+        hostname: process.env.MQTT_HOST_NAME,
+        username: process.env.MQTT_USER_NAME,
+        password: process.env.MQTT_PASSWORD
+    }
+};
