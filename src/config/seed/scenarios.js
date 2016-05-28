633740cd07afb600aa7f8bf66a307b4300a4ba43
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2016-05-28 17:54:39 +0300

    [TG-57] removed created seed scenarios

--- src/config/seed/scenarios.js
+++ src/config/seed/scenarios.js
@@ -2,9 +2,6 @@ import mongoose from 'mongoose';
 
 import scenarioModel from '../../scenarios/scenario.model';
 
-import scenario_01 from './scenario_01';
-import scenario_02 from './scenario_02';
-
 const scenarios = _createScenarios();
 
 export default { scenarios, populateScenarios };
@@ -18,20 +15,6 @@ function _createScenarios(){
         name: 'Greeting script',
         description: 'will greet you in a brand new world',
         body: 'console.log("Hello Scripto World!");'
-    }));
-
-    scenarios.push(scenarioModel({
-        _id: mongoose.Types.ObjectId('41224d776a326fb40f000003'),
-        name: 'Temperature tracking script',
-        description: 'will light a GPIO light when temperature rizes',
-        body: scenario_01
-    }));
-
-    scenarios.push(scenarioModel({
-        _id: mongoose.Types.ObjectId('41224d776a326fb40f000004'),
-        name: 'Lit tracking script',
-        description: 'will light both GPIO lights when light lowers',
-        body: scenario_02
     }));
 
     return scenarios;
