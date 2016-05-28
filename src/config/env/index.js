784dfb3bd6d594096338b4a2363719f08b279057
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2016-05-28 18:16:25 +0300

    [TG-57] restored config

--- src/config/env/index.js
+++ src/config/env/index.js
@@ -1,6 +1,6 @@
 import path from 'path';
 
-const env = process.env.NODE_ENV || 'cloud';
+const env = process.env.NODE_ENV || 'development';
 const config = require(`./${env}`);
 
 const defaults = {
