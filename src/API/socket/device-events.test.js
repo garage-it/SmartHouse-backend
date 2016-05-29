dad198365da852c5637f6ede074df4c3e549275a
Artem Trubitsyn <art.trubitsyn@gmail.com>
2016-05-29 23:22:08 +0300

    [TG-89][feat] Add switch sensor state, change sensor model, sync with FE

--- src/API/socket/device-events.test.js
+++ src/API/socket/device-events.test.js
@@ -1,4 +1,5 @@
 import chai from 'chai';
+import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import { expect } from 'chai';
 import proxyquire from 'proxyquire';
@@ -13,8 +14,13 @@ describe('# Socket Device Events', () => {
     let socket;
 
     beforeEach(function(){
+
+        socket = new socketIoMocks.socket();
+        sinon.spy(socket, 'on');
+
         input = {
-            stream: new Rx.Subject()
+            stream: new Rx.Subject(),
+            write: sinon.stub()
         };
 
         let sut = proxyquire('./device-events', {
@@ -23,18 +29,18 @@ describe('# Socket Device Events', () => {
 
         let io = socketIoMocks.server()();
         sut(io);
-        socket = io._connect();
+        io._connect(socket);
     });
 
     describe('# Event Subscription', () => {
 
-        it('will subscribe to specific device events', function(){
+        it('will subscribe to specific device events', () => {
             socket._handlers.subscribe({ device: 'a' });
             input.stream.next({ device: 'a', value: 1 });
             expect(socket.emit).to.have.been.calledWith('event', { device: 'a', value: 1 });
         });
 
-        it('will filter out other device events', function(){
+        it('will filter out other device events', () => {
             socket._handlers.subscribe({ device: 'a' });
             input.stream.next({ device: 'a', value: 1 });
             input.stream.next({ device: 'c', value: 2 });
@@ -42,13 +48,29 @@ describe('# Socket Device Events', () => {
             expect(socket.emit).not.to.have.been.calledWith('event', { device: 'c', value: 2 });
         });
 
-        it('will unsubscribe from events', function(){
+        it('will unsubscribe from events', () => {
             socket._handlers.subscribe({ device: 'a' });
             input.stream.next({ device: 'a', value: 1 });
             socket._handlers.unsubscribe({ device: 'a' });
             input.stream.next({ device: 'c', value: 2 });
             expect(socket.emit).not.to.have.been.calledWith('event', { device: 'a', value: 2 });
         });
+
+        it('will not subscribe to the publish event', () => {
+            socket._handlers.subscribe({ device: 'a' });
+            input.stream.next({ device: 'a', value: 1, publish: true });
+            expect(socket.emit).to.not.have.been.calledWith('event', { device: 'a', value: 1 });
+        });
+
+        it('will write event to the stream', () => {
+            let config = {
+                device: 'mockDev',
+                command: 'mockCommand'
+            };
+            socket.on.lastCall.args[1](config);
+            expect(input.write).to.have.been.called;
+        });
+
     });
 
 });
