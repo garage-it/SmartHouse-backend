4dadc70bf68d70cf58b74b289465077ee539aea4
Volodymyr Vreshch <Volodymyr_Vreshch@epam.com>
2016-09-20 19:03:24 +0300

    [TG_TECH][chore] improve runner

--- src/runner.js
+++ src/runner.js
@@ -1,4 +1,23 @@
- /* istanbul ignore next */
-require('babel-core/register');
+/* istanbul ignore next */
+var fs = require('fs');
+/* istanbul ignore next */
+var path = require('path');
+
+/* istanbul ignore next */
+var babelrc = fs.readFileSync(path.resolve(__dirname, '../.babelrc'));
+/* istanbul ignore next */
+var config;
+try {
+    /* istanbul ignore next */
+    config = JSON.parse(babelrc);
+} catch (err) {
+    /* eslint-disable *//* istanbul ignore next */
+    console.error('==> ERROR: Error parsing babelrc');
+    /* istanbul ignore next */
+    console.error(err);
+    /* eslint-enable */
+}
+/* istanbul ignore next */
+require('babel-core/register')(config);
 /* istanbul ignore next */
 require('./index.js');
