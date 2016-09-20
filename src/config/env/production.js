118369c6291cae53ae5ccd381ab36d50b08cfd62
Volodymyr Vreshch <Volodymyr_Vreshch@epam.com>
2016-09-20 12:08:53 +0300

    [TG-TEACH][chore] remove env prefixes

--- src/config/env/production.js
+++ src/config/env/production.js
@@ -1,15 +1,15 @@
 export default {
     env: 'production',
-    db: process.env.SH_MONGO_DB,
-    port: process.env.SH_WEB_PORT,
+    db: process.env.MONGO,
+    port: process.env.PORT,
     host: '0.0.0.0',
     seedDB: true,
-    staticPath: process.env.SH_PATH_FRONTENT_DIST,
+    staticPath: process.env.PATH_FRONTENT_DIST,
     plugAndPlay: false,
     mqtt: {
-        port: process.env.SH_MQTT_PORT,
-        hostname: process.env.SH_MQTT_HOST_NAME,
-        username: process.env.SH_MQTT_USER_NAME,
-        password: process.env.SH_MQTT_PASSWORD
+        port: process.env.MQTT_PORT,
+        hostname: process.env.MQTT_HOST_NAME,
+        username: process.env.MQTT_USER_NAME,
+        password: process.env.MQTT_PASSWORD
     }
 };
