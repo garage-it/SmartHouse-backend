279f84aa96b40a78ce1557a1e195e3f37b0ec23c
Artem Astakhov <artem_astakhov@epam.com>
2016-11-10 10:52:12 +0200

    [TG-246][feat] remove credantions for facebook

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,7 +1,6 @@
 export default {
     facebook: {
-        clientID:     '199769753762367',
-        clientSecret: '28e33acea5404ef12c2bdc85e802760e',
-        callbackURL:  'http://localhost:3000/api/auth/facebook/callback'
+        clientID:     'enter client id',
+        clientSecret: 'enter client secret'
     }
 };
