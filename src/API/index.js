dd2906c575cdcb72a4e6d4cf5f4d66bf97a9f821
Volodymyr Vreshch <vreshch@gmail.com>
2016-11-21 09:18:08 +0200

    feat(TG-277): timeseries (#71)
    * [TG-277] add mock data to backend
    
    * timeseries mock backend
    
    * lintfix
    
    * fix substraction
    
    * improve coverage
    
    * take into account comments

--- src/API/index.js
+++ src/API/index.js
@@ -1,27 +1,27 @@
-// NOTE: this is a starter kit file
-// TODO: refactor
-
 import express from 'express';
 
 import sensorRoutes from './sensors/sensor.routes';
 import scenarioRoutes from './scenarios/scenario.routes';
 import scenarioConverterRoutes from './scenario-converter/scenario-converter.routes.js';
 import dashboardRoutes from './dashboard/dashboard.routes';
+import timeseriesRoutes from './timeseries/timeseries.routes';
 import user from './user/user.routes';
 import auth from './auth/auth.routes';
 
 const router = express.Router();    // eslint-disable-line new-cap
 
 /** GET /health-check - Check service health */
-router.get('/health-check', (req, res) =>
-    res.send('OK')
-);
+router.get('/health-check', (req, res) => {
+    res.send('OK');
+});
 
 router.use('/sensors', sensorRoutes);
 router.use('/scenarios', scenarioRoutes);
 router.use('/scenario-converter', scenarioConverterRoutes);
 router.use('/dashboard', dashboardRoutes);
+router.use('/timeseries', timeseriesRoutes);
 router.use('/user', user);
 router.use('/auth', auth);
+
 
 export default router;
---
+++ src/API/timeseries/timeseries.controller.js
@@ -0,0 +1,65 @@
+import APIError from '../helpers/APIError';
+import httpStatus from 'http-status';
+import moment from 'moment';
+
+const PERIOD_TO_INTERVAL = {
+    'day': 60,
+    'week': 60,
+    'month': 60*24,
+    'year': 60*24
+};
+
+function query(req, res, next) {
+
+    if (!req.query.period || ! PERIOD_TO_INTERVAL.hasOwnProperty(req.query.period)) {
+        const err = new APIError('Period is not provided to wrong', httpStatus.BAD_REQUEST);
+        return next(err);
+    }
+
+    if (!req.query.sensor) {
+        const err = new APIError('Sensor is not provided', httpStatus.BAD_REQUEST);
+        return next(err);
+    }
+
+    const {sensor, period} = req.query;
+    const stepMin = PERIOD_TO_INTERVAL[period];
+    const from = moment();
+    const to = from.clone();
+
+    // Period
+    switch (period) {
+    case 'day':
+        from.subtract(1, 'day');
+        break;
+    case 'week':
+        from.subtract(7, 'day');
+        break;
+    case 'month':
+        from.subtract(1, 'month');
+        break;
+    case 'year':
+        from.subtract(1, 'year');
+        break;
+    }
+
+    const data = [];
+    let current = to.clone();
+
+    while (current >= from) {
+        data.push({
+            date: current.toString(),
+            value: Math.floor(Math.random()*100)
+        });
+        current = current.subtract(stepMin, 'minutes');
+    }
+
+    res.json({
+        from,
+        to,
+        sensor,
+        stepMin,
+        data
+    });
+}
+
+export default { query };
