2158c524136d848492520f088017a9c54cc8001c
Borys Ihnatyev <paur.link@gmail.com>
2016-11-23 02:48:07 +0200

    add name, description and active flag fields

--- src/API/map-view/map-view.model.js
+++ src/API/map-view/map-view.model.js
@@ -5,6 +5,12 @@ import moongose from 'mongoose';
  */
 export default moongose.model('MapView', new moongose.Schema({
     pictureName: String,
+    name: String,
+    description: String,
+    active: {
+        type: Boolean,
+        default: false
+    },
     sensors: [{
         sensor: {
             type: moongose.Schema.Types.ObjectId,
