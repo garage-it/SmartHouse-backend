dc0eebca552e3cc939b3b1d5eb4c59d0d54447ff
Borys Ihnatyev <paur.link@gmail.com>
2016-12-02 22:52:56 +0200

    move all-test runner under the runners folder

--- test/runners/unit.js
+++ test/runners/unit.js
@@ -4,9 +4,9 @@ require('babel-register');
 
 const
     path = require('path'),
-    load = require('./load');
+    load = require('../load');
 
-const SRC_PATH = path.join(__dirname, '../src');
+const SRC_PATH = path.resolve(__dirname, '../../src');
 const TEST_PATTERN = '.test.js';
 
 load(SRC_PATH, TEST_PATTERN);
