ba3b7af645e56c20a86f99847eb129a86dd4ecaf
Oleksandr_Hliebov <Oleksandr_Hliebov@epam.com>
2016-12-12 21:20:40 +0200

    sensors added

--- src/config/seed/devices.js
+++ src/config/seed/devices.js
@@ -1,18 +1,9 @@
-import mongoose from 'mongoose';
 import sensorModel from '../../API/sensors/sensor.model';
 
 export default { populateDevices };
 
 /* istanbul ignore next */
 const devices = [];
-
-devices.push(new sensorModel({
-    _id: mongoose.Types.ObjectId('41224d776a326fb40f000001'),
-    description: 'some description',
-    type: 'some type',
-    metrics: 'meters',
-    mqttId: 'distance'
-}));
 
 devices.push(new sensorModel({
     description: 'temperature',
