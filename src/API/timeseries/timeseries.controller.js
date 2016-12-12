3ee4bd29ec8d3090b65128cb0889929402c964c6
Oleksandr_Tsaruk <alex.zogich@yandex.ru>
2016-12-12 23:01:38 +0200

    feat(TG-405): add integration with caiman

--- src/API/timeseries/timeseries.controller.js
+++ src/API/timeseries/timeseries.controller.js
@@ -1,17 +1,18 @@
 import APIError from '../helpers/APIError';
 import httpStatus from 'http-status';
+import timeSeriesService from './timeseries.service';
 import moment from 'moment';
 
-const PERIOD_TO_INTERVAL = {
-    'day': 60,
-    'week': 60*24,
-    'month': 60*24,
-    'year': 60*24*30
+const CHILD_PERIODS = {
+    'day': 'hour',
+    'week': 'day',
+    'month': 'day',
+    'year': 'month'
 };
 
 function query(req, res, next) {
 
-    if (!req.query.period || ! PERIOD_TO_INTERVAL.hasOwnProperty(req.query.period)) {
+    if (!req.query.period || ! CHILD_PERIODS.hasOwnProperty(req.query.period)) {
         const err = new APIError('Period is not provided to wrong', httpStatus.BAD_REQUEST);
         return next(err);
     }
@@ -22,46 +23,17 @@ function query(req, res, next) {
     }
 
     const {sensor, period} = req.query;
-    const stepMin = PERIOD_TO_INTERVAL[period];
-    const from = moment();
-    const to = from.clone();
+    const devices = timeSeriesService.getDevicesStatistic();
+    const from = moment().subtract(1, period);
 
-    // Period
-    switch (period) {
-    case 'day':
-        from.subtract(1, 'day');
-        break;
-    case 'week':
-        from.subtract(7, 'day');
-        break;
-    case 'month':
-        from.subtract(1, 'month');
-        break;
-    case 'year':
-        from.subtract(1, 'year');
-        break;
-    }
-
-    const data = [];
-    let current = to.clone();
-
-    while (current >= from) {
-        data.push({
-            date: current.toString(),
-            value: Math.floor(Math.random()*100)
-        });
-        current = current.subtract(stepMin, 'minutes');
-    }
-
-    data.reverse();
-
-    res.json({
-        from,
-        to,
-        sensor,
-        stepMin,
-        data
-    });
+    devices[sensor].getCollection(from.toDate(), CHILD_PERIODS[period]).toArray()
+        .then(data => {
+            res.json({
+                sensor,
+                data: data.map(item => ({ date: item.startDate, value: item.data.average }))
+            });
+        })
+        .catch(next);
 }
 
 export default { query };
