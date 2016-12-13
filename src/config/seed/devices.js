9344e3b2b7cf975b8b0b20f5c45f3a6d61ee4e20
Oleksandr_Hliebov <Oleksandr_Hliebov@epam.com>
2016-12-13 12:07:02 +0200

    few more sensors added

--- src/config/seed/devices.js
+++ src/config/seed/devices.js
@@ -7,16 +7,51 @@ const devices = [];
 
 devices.push(new sensorModel({
     description: 'temperature',
-    type: 'some other type',
+    type: 'sensor',
     metrics: 'celsius',
     mqttId: 'temperature'
 }));
 
 devices.push(new sensorModel({
     description: 'humidity',
-    type: 'some other type',
+    type: 'sensor',
     metrics: 'percents',
     mqttId: 'humidity'
+}));
+
+devices.push(new sensorModel({
+    description: 'co2',
+    type: 'sensor',
+    metrics: 'percents',
+    mqttId: 'co2'
+}));
+
+devices.push(new sensorModel({
+    description: 'pressure',
+    type: 'sensor',
+    metrics: 'percents',
+    mqttId: 'pressure'
+}));
+
+devices.push(new sensorModel({
+    description: 'electricity',
+    type: 'switcher',
+    executor: false,
+    mqttId: 'electricity'
+}));
+
+devices.push(new sensorModel({
+    description: 'socket',
+    type: 'switcher',
+    executor: false,
+    mqttId: 'socket'
+}));
+
+devices.push(new sensorModel({
+    description: 'lock',
+    type: 'switcher',
+    executor: false,
+    mqttId: 'lock'
 }));
 
 devices.push(new sensorModel({
@@ -25,13 +60,6 @@ devices.push(new sensorModel({
     metrics: 'degrees',
     mqttId: 'servo',
     servo: true
-}));
-
-devices.push(new sensorModel({
-    description: 'switcher',
-    type: 'some other type',
-    metrics: 'boolean',
-    mqttId: 'switcher'
 }));
 
 function populateDevices() {
