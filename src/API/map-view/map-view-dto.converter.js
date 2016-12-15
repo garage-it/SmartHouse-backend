57229deca4919fec9b9334d52896633bd346fbcc
Viktor Novak <valakar@gmail.com>
2016-12-15 09:21:24 +0200

    feat(TG-430): fix lint

--- src/API/map-view/map-view-dto.converter.js
+++ src/API/map-view/map-view-dto.converter.js
@@ -8,7 +8,6 @@ const mapViewDtoConverter = {
 export default mapViewDtoConverter;
 
 function create({ name, description, active, defaultView, sensors }) {
-    // console.log(123, name, description, active, default, sensors);
     return _.omitBy(
         { name, description, active, default: defaultView, sensors },
         (value) => _.isUndefined(value)
