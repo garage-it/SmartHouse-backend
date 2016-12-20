5e523b4ee65b95155c9300b1fa1c89fa7aa86587
Viktor Novak <valakar@gmail.com>
2016-12-20 14:36:46 +0200

    feat(TG-434): fix api routes

--- src/API/index.js
+++ src/API/index.js
@@ -3,7 +3,6 @@ import express from 'express';
 import sensorRoutes from './sensors/sensor.routes';
 import scenarioRoutes from './scenarios/scenario.routes';
 import scenarioConverterRoutes from './scenario-converter/scenario-converter.routes.js';
-import dashboardRoutes from './dashboard/dashboard.routes';
 import timeseriesRoutes from './timeseries/timeseries.routes';
 import switcherStatisticsRoutes from './switcher-statistics/switcher-statistics.router';
 import user from './user/user.routes';
@@ -25,7 +24,6 @@ router.get('/health-check', (req, res) => {
 router.use('/sensors', sensorRoutes);
 router.use('/scenarios', scenarioRoutes);
 router.use('/scenario-converter', scenarioConverterRoutes);
-router.use('/dashboard', dashboardRoutes);
 router.use('/timeseries', timeseriesRoutes);
 router.use('/switcher-statistics', switcherStatisticsRoutes);
 router.use('/user', user);
