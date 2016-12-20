280c47659b25443478189cdd1c0fb9e7ea28d757
Appius <appius@outlook.com>
2016-12-20 17:20:23 +0200

    feat(TG-434) Removed unused imports

--- src/API/view/view.controller.js
+++ src/API/view/view.controller.js
@@ -1,9 +1,4 @@
-import ViewModel from './view.model';
 import viewService from './view.service';
-
-import MapViewModel from '../map-view/map-view.model';
-import DashboardViewModel from '../dashboard-view/dashboard-view.model';
-import SensorModel from '../sensors/sensor.model';
 
 export default {
     query,
